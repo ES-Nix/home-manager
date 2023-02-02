71672b1a296ef5c1570d56a1f950c95ecd63d358
Pedro <pedroalencarregis@hotmail.com>
2023-02-02 05:00:23 -0300

    Tests programs.zsh WIP starship c

--- home.nix
+++ home.nix
@@ -356,18 +356,18 @@
           ];
           detect_folders = [];
         };
-#        c = {
-#          format = "[$symbol($version(-$name) )]($style)";
-#          version_format = "v$raw";
-#          style = "fg:149 bold bg:0x86BBD8";
-#          symbol = " ";
-#          disabled = false;
-#          detect_extensions = [
-#            "c"
-#            "h"
-#          ];
-#          detect_files = [];
-#          detect_folders = [];
+        c = {
+          format = "[$symbol($version(-$name) )]($style)";
+          version_format = "v$raw";
+          style = "fg:149 bold bg:0x86BBD8";
+          symbol = " ";
+          disabled = false;
+          detect_extensions = [
+            "c"
+            "h"
+          ];
+          detect_files = [];
+          detect_folders = [];
 #          commands = [
 #            [
 #            "cc"
@@ -382,7 +382,7 @@
 #            "--version"
 #            ]
 #          ];
-#        };
+        };
         character = {
           format = "$symbol ";
           vicmd_symbol = "[❮](bold green)";
