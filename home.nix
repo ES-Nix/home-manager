c2bc05b17a5f537c39cbaf86e8370560c9447ef0
Pedro <pedroalencarregis@hotmail.com>
2023-02-02 04:54:22 -0300

    Tests programs.zsh WIP starship

--- home.nix
+++ home.nix
@@ -253,8 +253,8 @@
             "sudo"
             "systemadmin" # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/systemadmin
             "systemd"
-            # "terraform"
-            "thefuck"
+            "terraform"
+            # "thefuck"
             "tig"
             "timer"
             "tmux"
@@ -300,17 +300,19 @@
 #    };
 #  };
 
-#  programs.starship = {
-#      enable = true;
-#      settings = {
-#        # "$schema" = "https://starship.rs/config-schema.json";
-#        add_newline = true;
-#        command_timeout = 500;
-#        continuation_prompt = "[∙](bright-black) ";
-#        format = "[](0x9A348E)$username$hostname$localip$shlvl$singularity$kubernetes[](fg:0x9A348E bg:0xDA627D)$directory$vcsh[](fg:0xDA627D bg:0xFCA17D)$git_branch$git_commit$git_state$git_metrics$git_status$hg_branch[](fg:0x86BBD8 bg:0x06969A)$docker_context$package$buf[](fg:0xFCA17D bg:0x86BBD8)$c$cmake$cobol$container$daml$dart$deno$dotnet$elixir$elm$erlang$golang$haskell$helm$java$julia$kotlin$lua$nim$nodejs$ocaml$perl$php$pulumi$purescript$python$rlang$red$ruby$rust$scala$swift$terraform$vlang$vagrant$zig$nix_shell$conda$spack$memory_usage$aws$gcloud$openstack$azure$env_var$crystal$custom$sudo$cmd_duration$line_break$jobs$battery[](fg:0x06969A bg:0x33658A)$time$status$shell$character";
-#        right_format = "";
-#        scan_timeout = 30;
-#
+  programs.starship = {
+      enable = true;
+      enableZshIntegration = true;
+      settings = {
+        # "$schema" = "https://starship.rs/config-schema.json";
+        add_newline = true;
+        command_timeout = 500;
+        continuation_prompt = "[∙](bright-black) ";
+        format = "[](0x9A348E)$username$hostname$localip$shlvl$singularity$kubernetes[](fg:0x9A348E bg:0xDA627D)$directory$vcsh[](fg:0xDA627D bg:0xFCA17D)$git_branch$git_commit$git_state$git_metrics$git_status$hg_branch[](fg:0x86BBD8 bg:0x06969A)$docker_context$package$buf[](fg:0xFCA17D bg:0x86BBD8)$c$cmake$cobol$container$daml$dart$deno$dotnet$elixir$elm$erlang$golang$haskell$helm$java$julia$kotlin$lua$nim$nodejs$ocaml$perl$php$pulumi$purescript$python$rlang$red$ruby$rust$scala$swift$terraform$vlang$vagrant$zig$nix_shell$conda$spack$memory_usage$aws$gcloud$openstack$azure$env_var$crystal$custom$sudo$cmd_duration$line_break$jobs$battery[](fg:0x06969A bg:0x33658A)$time$status$shell$character";
+        right_format = "";
+        scan_timeout = 30;
+      };
+  };
 #        aws = {
 #          format = "[$symbol($profile )(($region) )([$duration] )]($style)";
 #          symbol = "🅰 ";
